412dd1b09dbb2061d4c42f7b682f5a69bf538ba9
red-drive <abhinandd.me@gmail.com>
2021-09-28 19:11:11 +0530

    Kind of wierd but I have only two branch

--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 pb_key = os.environ.get('PB_KEY')
 
-Sales_Old = 0 # (Sale)For Recording old transaction to compare to next transaction
-Sales_Return_Old = 0 # (Sales Return)For Recording old transaction to compare to next transaction
+P_Sales_Old = 0 # (Sale)For Recording old transaction to compare to next transaction
+K_Sales_Old = 0
+P_Sales_Return_Old = 0 # (Sales Return)For Recording old transaction to compare to next transaction
+K_Sales_Return_Old = 0
 
 try:
     push = PushBullet(pb_key)
@@ -21,25 +23,43 @@ def hello():
 
 @app.route("/sales/<salesamount>/<location>") 
 def sales_happened(salesamount,location):
-    global Sales_Old
+    global P_Sales_Old,K_Sales_Old
     sale = int(salesamount) # String can't be used to compare with integer
     loc = location
-    if sale > Sales_Old: # This happen when the new sale is higher than old sale
-        print("Current Sale : "+str(sale-Sales_Old)) # Gives us the difference
-        print("Location : "+loc) # This will explain the location thing, where is the f***g shop
-        try:
-            push.push_note(title="Sales in "+loc,body="Amount of Rp. "+str(sale-Sales_Old))
-        except:
-            pass
-        Sales_Old = sale # Update the new transaction to Sales Old because we need to be updated
-    elif sale < Sales_Old: # This happens when the old sale is higher than new sale
-        print("Sale Deleted : "+str(Sales_Old-sale)) # Gives us the difference
-        print("Location : "+loc) # This will explain the location thing, where is the f***g shop
-        try:
-            push.push_note(title="Sales Del in "+loc,body="Amount of Rp. "+str(Sales_Old-sale))
-        except:
-            pass
-        Sales_Old = sale # Update the new transaction to Sales Old because we need to be updated
+    if location == "Pulliparakunnu":
+        if sale > P_Sales_Old: # This happen when the new sale is higher than old sale
+            print("Current Sale : "+str(sale-P_Sales_Old)) # Gives us the difference
+            print("Location : "+loc) # This will explain the location thing, where is the f***g shop
+            try:
+                push.push_note(title="Sales in "+loc,body="Amount of Rp. "+str(sale-P_Sales_Old))
+            except:
+                pass
+            P_Sales_Old = sale # Update the new transaction to Sales Old because we need to be updated
+        elif sale < P_Sales_Old: # This happens when the old sale is higher than new sale
+            print("Sale Deleted : "+str(P_Sales_Old-sale)) # Gives us the difference
+            print("Location : "+loc) # This will explain the location thing, where is the f***g shop
+            try:
+                push.push_note(title="Sales Del in "+loc,body="Amount of Rp. "+str(P_Sales_Old-sale))
+            except:
+                pass
+            P_Sales_Old = sale # Update the new transaction to Sales Old because we need to be updated
+    elif location == "Kalletumkara":
+        if sale > K_Sales_Old:  # This happen when the new sale is higher than old sale
+            print("Current Sale : " + str(sale - K_Sales_Old))  # Gives us the difference
+            print("Location : " + loc)  # This will explain the location thing, where is the f***g shop
+            try:
+                push.push_note(title="Sales in " + loc, body="Amount of Rp. " + str(sale - K_Sales_Old))
+            except:
+                pass
+            P_Sales_Old = sale  # Update the new transaction to Sales Old because we need to be updated
+        elif sale < K_Sales_Old:  # This happens when the old sale is higher than new sale
+            print("Sale Deleted : " + str(K_Sales_Old - sale))  # Gives us the difference
+            print("Location : " + loc)  # This will explain the location thing, where is the f***g shop
+            try:
+                push.push_note(title="Sales Del in " + loc, body="Amount of Rp. " + str(K_Sales_Old - sale))
+            except:
+                pass
+            K_Sales_Old = sale  # Update the new transaction to Sales Old because we need to be updated
     else: # If the updated details are not high and not low and same this will be returned
         print("You in Serious Trouble, lol nothing serious")
     return "Sales Updated"
@@ -48,25 +68,43 @@ def sales_happened(salesamount,location):
 #  I haven't added guide over here I'm soo lazy.
 @app.route("/salesreturn/<salesreturnamount>/<location>")
 def sales_return_happened(salesreturnamount,location):
-    global Sales_Return_Old
+    global P_Sales_Return_Old,K_Sales_Return_Old
     sale_return = int(salesreturnamount)
     loc = location
-    if sale_return > Sales_Return_Old:
-        print("Current Sales Return : "+str(sale_return-Sales_Return_Old))
-        print("Location : "+loc)
-        try:
-            push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(sale_return-Sales_Return_Old))
-        except:
-            pass
-        Sales_Return_Old = sale_return
-    elif sale_return < Sales_Return_Old:
-        print("Sales Return Deleted : "+str(Sales_Return_Old-sale_return))
-        print("Location : "+loc)
-        try:
-            push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(Sales_Return_Old-sale_return))
-        except:
-            pass
-        Sales_Return_Old = sale_return
+    if location == "Pulliparakunnu":
+        if sale_return > P_Sales_Return_Old:
+            print("Current Sales Return : "+str(sale_return-P_Sales_Return_Old))
+            print("Location : "+loc)
+            try:
+                push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(sale_return-P_Sales_Return_Old))
+            except:
+                pass
+            P_Sales_Return_Old = sale_return
+        elif sale_return < P_Sales_Return_Old:
+            print("Sales Return Deleted : "+str(P_Sales_Return_Old-sale_return))
+            print("Location : "+loc)
+            try:
+                push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(P_Sales_Return_Old-sale_return))
+            except:
+                pass
+            P_Sales_Return_Old = sale_return
+    elif location == "Kalletumkara":
+        if sale_return > K_Sales_Return_Old:
+            print("Current Sales Return : "+str(sale_return-K_Sales_Return_Old))
+            print("Location : "+loc)
+            try:
+                push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(sale_return-K_Sales_Return_Old))
+            except:
+                pass
+            K_Sales_Return_Old = sale_return
+        elif sale_return < K_Sales_Return_Old:
+            print("Sales Return Deleted : "+str(K_Sales_Return_Old-sale_return))
+            print("Location : "+loc)
+            try:
+                push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(K_Sales_Return_Old-sale_return))
+            except:
+                pass
+            K_Sales_Return_Old = sale_return
     else:
         print("You in a serious trouble")
     return("Sales Return Updated")
