63f856fe44fa8a3fa3bd59a91c20ede35299fd0c
red-drive <abhinandd.me@gmail.com>
2021-09-28 18:19:58 +0530

    Preparing to Host in heroku
    I'm a crazy man, This I made gut.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # I'm a N00b I have used print instead of logging module.
 from flask import Flask
+import os
+from  pushbullet import PushBullet
 
 app = Flask(__name__)
+pb_key = os.environ.get("PB_KEY")
 
 Sales_Old = 0 # (Sale)For Recording old transaction to compare to next transaction
 Sales_Return_Old = 0 # (Sales Return)For Recording old transaction to compare to next transaction
+
+push = PushBullet(pb_key)
 
 @app.route("/sales/<salesamount>/<location>") 
 def sales_happened(salesamount,location):
@@ -14,10 +19,12 @@ def sales_happened(salesamount,location):
     if sale > Sales_Old: # This happen when the new sale is higher than old sale
         print("Current Sale : "+str(sale-Sales_Old)) # Gives us the difference
         print("Location : "+loc) # This will explain the location thing, where is the f***g shop
+        push.push_note(title="Sales in "+loc,body="Amount of Rp. "+str(sale-Sales_Old))
         Sales_Old = sale # Update the new transaction to Sales Old because we need to be updated
     elif sale < Sales_Old: # This happens when the old sale is higher than new sale
         print("Sale Deleted : "+str(Sales_Old-sale)) # Gives us the difference
         print("Location : "+loc) # This will explain the location thing, where is the f***g shop
+        push.push_note(title="Sales Del in "+loc,body="Amount of Rp. "+str(Sales_Old-sale))
         Sales_Old = sale # Update the new transaction to Sales Old because we need to be updated
     else: # If the updated details are not high and not low and same this will be returned
         print("You in Serious Trouble, lol nothing serious")
@@ -33,14 +40,16 @@ def sales_return_happened(salesreturnamount,location):
     if sale_return > Sales_Return_Old:
         print("Current Sales Return : "+str(sale_return-Sales_Return_Old))
         print("Location : "+loc)
+        push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(sale_return-Sales_Return_Old))
         Sales_Return_Old = sale_return
     elif sale_return < Sales_Return_Old:
         print("Sales Return Deleted : "+str(Sales_Return_Old-sale_return))
         print("Location : "+loc)
+        push.push_note(title="Return in "+loc,body="Amount of Rp. "+str(Sales_Return_Old-sale_return))
         Sales_Return_Old = sale_return
     else:
         print("You in a serious trouble")
     return("Sales Return Updated")
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5500)
+    app.run(debug=True,host='0.0.0.0')
